6dd4d973a3bad054a9514ecea1b4f1dd811f25bd
Adedamola-Yusuff <adedamolayusuff341@gmail.com>
2025-08-23 16:32:36 +0200

    Debugged some stuff. Changed MOV to MOVS a bunch of times. Changed 'TST Rx, #imm' to 'MOVS Ry, #imm' + 'TST Ry, Rx' a bunch of times

--- Practical2/Core/Src/assembly.s
+++ Practical2/Core/Src/assembly.s
@@ -38,23 +38,27 @@ main_loop:
 	LDRB R3, [R0, #0x10]    @ Loads the state of bits 0 to 7 into R3 from the IDR every loop cycle. We only care about buttons SW0 to SW3 though
 	MVNS R3, R3             @ Inverts R3. Necessary because a button being pressed sends a logic 0 but it's much easier to work with 1s
 
-	TST R3, #8              @ Checks bit 3 of the IDR to see if SW3 is being pressed. Sets Z flag to 0 (ANDS result = 0b00000001) if SW0 is pressed and Z flag to 1 (ANDS result = 0b00000000) if SW0 isn't pressed
+	MOVS R5, #8              @ New line
+	TST R3, R5              @ Checks bit 3 of the IDR to see if SW3 is being pressed. Sets Z flag to 0 (ANDS result = 0b00000001) if SW0 is pressed and Z flag to 1 (ANDS result = 0b00000000) if SW0 isn't pressed
 	BNE freeze              @ Skips all the code that would change the pattern of the LEDs if SW3 is pressed
 
-    TST R3, #4              @ Checks bit 2 of the IDR to see if SW2 is being pressed
+	MOVS R5, #4              @ New line
+    TST R3, R5              @ Checks bit 2 of the IDR to see if SW2 is being pressed
     BEQ dont_set_pattern    @ If Z=1, don't set pattern. If Z=0, set pattern
-    MOV R2, #0xAA           @ Sets the pattern of the LEDs
+    MOVS R2, #0xAA     @ Sets the pattern of the LEDs
     B write_leds            @ Unconditional branching to write the pattern to the LEDs
 
 dont_set_pattern:           @ If the program reaches this point, then the 0xAA pattern was not set
-	TST R3, #1              @ Checks bit 0 of the IDR to see if SW0 Is being pressed
+	MOVS R5, #1              @ New line
+	TST R3, R5              @ Checks bit 0 of the IDR to see if SW0 Is being pressed
 
 	BEQ increment_by_one    @ This and next three lines increment the pattern by one if SW0 is not pressed and by 2 if SW0 is pressed
 	ADDS R2, R2, #1
 increment_by_one:
 	ADDS R2, R2, #1
 
-	TST R3, #2              @ Checks if SW1 is being pressed
+	MOVS R5, #2
+	TST R3, R5              @ Checks if SW1 is being pressed
     BNE short_delay         @ Makes it so that the loop_delay is going to use SHORT_DELAY_CNT because SW1 is not being pressed
     LDR R4, LONG_DELAY_CNT  @ If we're here, then we're not using SHORT_DELAY, which means that we want to use LONG_DELAY_CNT
     B loop_delay            @ Makes sure that we skip short_delay and immediately implement the delay
